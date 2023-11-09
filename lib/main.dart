dc9fee6629d436e640169fb2c0eb1fc88806b7e7
NFalah25 <falahnaufal1@gmail.com>
2023-11-09 21:56:02 +0700

    UTS

--- lib/main.dart
+++ lib/main.dart
@@ -20,70 +20,70 @@ class MyApp extends StatelessWidget {
           title: const Text("Tri Jagad Ariyani | 2141720049"),
         ),
         body: Container(
-            padding: const EdgeInsets.all(20),
-            margin: const EdgeInsets.all(20),
-            color: const Color.fromARGB(42, 233, 30, 98),
-            child: Column(
-              children: [
-                Expanded(
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          width: 200,
-                          height: 200,
-                          color: Colors.pink,
-                          alignment: Alignment.center,
-                          child: const Text(
-                            "Pink",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                            ),
+          padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.all(20),
+          color: const Color.fromARGB(42, 233, 30, 98),
+          child: Column(
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        width: 200,
+                        height: 200,
+                        color: Colors.pink,
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "Pink",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                Expanded(
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          width: 200,
-                          height: 300,
-                          color: Colors.teal,
-                          alignment: Alignment.center,
-                          child: const Text(
-                            "Teal",
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white,
-                            ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        width: 200,
+                        height: 300,
+                        color: Colors.teal,
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "Teal",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
                           ),
                         ),
                       ),
-                      Expanded(
-                        child: Container(
-                          width: 200,
-                          height: 300,
-                          color: Colors.yellow,
-                          alignment: Alignment.center,
-                          child: const Text(
-                            "Yellow",
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.black,
-                            ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        width: 200,
+                        height: 300,
+                        color: Colors.yellow,
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "Yellow",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                Expanded(
-                    child: Row(
+              ),
+              Expanded(
+                child: Row(
                   children: [
                     Expanded(
                       child: Container(
@@ -116,84 +116,92 @@ class MyApp extends StatelessWidget {
                       ),
                     ),
                     Expanded(
-                        child: Container(
-                      width: 200,
-                      height: 300,
-                      color: Colors.indigoAccent,
-                      alignment: Alignment.center,
-                      child: const Text(
-                        "Indigo",
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.white,
+                      child: Container(
+                        width: 200,
+                        height: 300,
+                        color: Colors.indigoAccent,
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "Indigo",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    )),
+                    ),
                   ],
-                )),
-                Expanded(
-                    child: Row(
+                ),
+              ),
+              Expanded(
+                child: Row(
                   children: [
                     Expanded(
-                        child: Container(
-                      width: 200,
-                      height: 300,
-                      color: Colors.pink,
-                      alignment: Alignment.center,
-                      child: const Text(
-                        "Pink",
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.white,
+                      child: Container(
+                        width: 200,
+                        height: 300,
+                        color: Colors.pink,
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "Pink",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    )),
+                    ),
                     Expanded(
-                        child: Container(
-                      width: 200,
-                      height: 300,
-                      color: Colors.blueAccent,
-                      alignment: Alignment.center,
-                      child: const Text(
-                        "Blue",
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.white,
+                      child: Container(
+                        width: 200,
+                        height: 300,
+                        color: Colors.blueAccent,
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "Blue",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    )),
+                    ),
                     Expanded(
-                        child: Container(
-                      width: 200,
-                      height: 300,
-                      color: Colors.purpleAccent,
-                      alignment: Alignment.center,
-                      child: const Text(
-                        "Purple",
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.white,
+                      child: Container(
+                        width: 200,
+                        height: 300,
+                        color: Colors.purpleAccent,
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "Purple",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    )),
+                    ),
                     Expanded(
-                        child: Container(
-                      width: 200,
-                      height: 300,
-                      color: Colors.greenAccent,
-                      alignment: Alignment.center,
-                      child: const Text(
-                        "Green",
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.red,
+                      child: Container(
+                        width: 200,
+                        height: 300,
+                        color: Colors.greenAccent,
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "Green",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.red,
+                          ),
                         ),
                       ),
-                    )),
+                    ),
                   ],
-                )),
-              ],
-            )),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
